6a75a65572ce2d6bcadc3b0977f8c0104058f025
Britton Smith <brittonsmith@gmail.com>
2015-10-18 21:52:36 +0100

    Adding a setup.py and __init__.py for importing.

---
+++ grid_figure/__init__.py
@@ -0,0 +1,1 @@
+from grid_figure import GridFigure
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(name="grid_figure",
+      version="1.0",
+      description="A utility to set up matplotlib axes for multi-panel plots.",
+      author="Britton Smith",
+      author_email="brittonsmith@gmail.com",
+      license="BSD",
+      url="http://bitbucket.org/brittonsmith/grid_figure",
+      packages=["grid_figure"],
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Console",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: POSIX :: AIX",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Utilities",
+          ],
+      )
